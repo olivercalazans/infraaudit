627ac422f09cd1766c34bd2370c6ceba45c4e7a0
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-04-30 17:44:22 -0300

    add function to perform snmpget

--- src/infraaudit/snmp/snmp_fetcher.py
+++ src/infraaudit/snmp/snmp_fetcher.py
@@ -4,7 +4,10 @@
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software...
 
 
-import pysnmp
+from pysnmp.hlapi.v1arch    import get_cmd, CommunityData
+from pysnmp.hlapi.transport import SnmpEngine
+from pysnmp.hlapi.v3arch    import UdpTransportTarget, ContextData, ObjectType, ObjectIdentity
+import secrets.snmp_secrets as snmp_secrets
 
 
 class SNMP_Fetcher:
@@ -17,11 +20,12 @@ class SNMP_Fetcher:
         return cls._instance
 
 
+
     __slots__ = ('_sysObjectID', '_ip')
 
     def __init__(self):
         self._sysObjectID:str = '1.3.6.1.2.1.1.2.0'
-        self._ip:str = None
+        self._ip:str          = None
 
 
 
@@ -31,3 +35,28 @@ class SNMP_Fetcher:
 
     def __exit__(self, exc_type, exc_value, traceback):
         return False
+    
+
+
+    @staticmethod
+    def _get_info_by_snmpget(ip:str, oid:str) -> str:
+        errorIndication, errorStatus, errorIndex, varBinds = next(
+            get_cmd(
+                SnmpEngine(),
+                CommunityData(snmp_secrets.COMMUNITY, mpModel=1),
+                UdpTransportTarget((ip, 161)),
+                ContextData(),
+                ObjectType(ObjectIdentity(oid))
+            )
+        )
+        if errorIndication:
+            print(f'Erro: {errorIndication}')
+        elif errorStatus:
+            print(f'{errorStatus.prettyPrint()} at {errorIndex and varBinds[int(errorIndex)-1][0] or "?"}')
+        else:
+            for varBind in varBinds:
+                print(f'{varBind[0]} = {varBind[1]}')
+
+
+
+SNMP_Fetcher()._get_info_by_snmpget('192.168.207.108', '1.3.6.1.2.1.1.2.0')
