319508442f79cdbb93773efaf8b5dbb9a9665380
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-09-23 17:50:00 -0300

    displays data from zabbix fetcher

--- main.go
+++ main.go
@@ -8,12 +8,17 @@ import (
 
 func main() {
 	device := internal.Data{}
-	jsonData, err := internal.GetDataFromZabbix()
+	hosts, err := internal.GetDataFromZabbix()
+
 	if err != nil {
-		fmt.Println("Error fetching data:", err)
-		return
+		panic(err)
 	}
 
-	fmt.Println(string(jsonData))
+	for _, h := range hosts {
+		if len(h.Interfaces) > 0 {
+			fmt.Printf("ID: %s | Name: %s | IP: %s\n", h.HostID, h.Name, h.Interfaces[0].IP)
+		}
+	}
+
 	fmt.Print(device)
 }
