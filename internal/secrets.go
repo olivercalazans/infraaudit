3cd2f06e22d2f49070513a17f8dd060544dd1563
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-09-26 17:43:43 -0300

    added secrets file

---
+++ internal/secrets.go
@@ -0,0 +1,30 @@
+package internal
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+
+type Secrets struct {
+	APIURL string   `json:"api_url"`
+	IPS    []string `json:"ips"`
+}
+
+
+
+func NewSecrets() *Secrets {
+	file, err := os.Open("secrets.json")
+	if err != nil {
+		panic(fmt.Sprintf("failed to open config file: %v", err))
+	}
+	defer file.Close()
+
+	var secrets Secrets
+	if err := json.NewDecoder(file).Decode(&secrets); err != nil {
+		panic(fmt.Sprintf("failed to load config: %v", err))
+	}
+
+	return &secrets
+}
