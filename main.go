0d07fe968c74746344c546b49ef05815bd6d1d36
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-09-29 15:54:38 -0300

    bug fixed in the main function

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/olivercalazans/infraaudit/internal"
 )
 
 
 
 func main() {
-	secrets := internal.Secrets{}
-	device  := internal.Data{}
+	secrets := internal.NewSecrets()
+	//device  := internal.Data{}
 	hosts   := internal.GetDataFromZabbix(secrets.APIURL)
+
+	for _, item := range hosts {
+		fmt.Println(item)
+	}
 }
